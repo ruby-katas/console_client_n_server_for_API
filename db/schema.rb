233ebf0c90cf1ac43a78e6d33b07738d85e26d22
Pedro Alvarado <pedro.alvarado@globant.com>
2015-10-19 15:20:57 -0500

    Make models token authenticatable add acts_as_token_authenticatable to User model rails g migration add_authentication_token_to_users authentication_token:string:index rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019200801) do
+ActiveRecord::Schema.define(version: 20151019201724) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
@@ -34,8 +34,10 @@ ActiveRecord::Schema.define(version: 20151019200801) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
